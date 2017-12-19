e292ea484f3e5efdb540cb7da5ce6aa2f12ffa7c
tabuechn <tabuechn@htwg-konstanz.de>
2017-12-19 15:14:52 +0100

    changed version to import api

--- app/controllers/BattleshipController.scala
+++ app/controllers/BattleshipController.scala
@@ -2,12 +2,15 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
+import de.htwg.se.battleship.controller.Controller
+import de.htwg.se.battleship.view.TuiView
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 @Singleton
 class BattleshipController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
 
   def index = Action {
+    val con = new Controller(10, new TuiView)
     Ok(views.html.game(10,10))
   }
 }
--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 resolvers += Resolver.sonatypeRepo("snapshots")
 resolvers += Resolver.mavenLocal
 
-scalaVersion := "2.12.4"
+scalaVersion := "2.11.8"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
