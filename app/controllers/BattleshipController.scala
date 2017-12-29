86629676c277915b1675eee2942f9079b8c274fa
tabuechn <tabuechn@htwg-konstanz.de>
2017-12-29 13:59:05 +0100

    removed sockets

--- app/controllers/BattleshipController.scala
+++ app/controllers/BattleshipController.scala
@@ -9,25 +9,14 @@ import play.api.mvc.{AbstractController, ControllerComponents}
 import play.api.mvc._
 import akka.stream.scaladsl._
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
 @Singleton
 class BattleshipController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
 
+  val controller: Controller = Controller(10, new TuiView())
+
   def index = Action {
-    val con = new Controller(10, new TuiView)
-    Ok(views.html.game(10,10))
-  }
 
-  def socket = WebSocket.accept[String, String] { request =>
-
-    // Log events to the console
-    val in = Sink.foreach[String](println)
-
-    // Send a single 'Hello!' message and then leave the socket open
-    val out = Source.single("Hello!").concat(Source.maybe)
-
-    Flow.fromSinkAndSource(in, out)
+    Ok(views.html.game(controller.fieldSize,controller.fieldSize))
   }
 
 }
---
+++ public/javascripts/battleship.js
@@ -0,0 +1,8 @@
+$('document').ready(function(){
+    console.log("starting battleship game");
+    start();
+});
+
+function start() {
+
+}
