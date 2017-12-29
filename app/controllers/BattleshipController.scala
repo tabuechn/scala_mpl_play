1d43b83993184850c046abe8217e0ba51acc7cd3
tabuechn <tabuechn@htwg-konstanz.de>
2017-12-29 11:19:54 +0100

    scaled the field

--- app/controllers/BattleshipController.scala
+++ app/controllers/BattleshipController.scala
@@ -6,6 +6,11 @@ import de.htwg.se.battleship.controller.Controller
 import de.htwg.se.battleship.view.TuiView
 import play.api.mvc.{AbstractController, ControllerComponents}
 
+import play.api.mvc._
+import akka.stream.scaladsl._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
 @Singleton
 class BattleshipController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
 
@@ -13,4 +18,16 @@ class BattleshipController @Inject()(cc: ControllerComponents) extends AbstractC
     val con = new Controller(10, new TuiView)
     Ok(views.html.game(10,10))
   }
+
+  def socket = WebSocket.accept[String, String] { request =>
+
+    // Log events to the console
+    val in = Sink.foreach[String](println)
+
+    // Send a single 'Hello!' message and then leave the socket open
+    val out = Source.single("Hello!").concat(Source.maybe)
+
+    Flow.fromSinkAndSource(in, out)
+  }
+
 }
